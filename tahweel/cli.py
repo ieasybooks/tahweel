96cc61cf8daf9c5cb7c898ac23d134bb77f6def5
Ali Hamdi Ali Fadel <aliosm1997@gmail.com>
2024-07-19 20:52:27 +0000

    Utilize --dir-output-type argument in PdfFileManager and cli.py

--- tahweel/cli.py
+++ tahweel/cli.py
@@ -33,17 +33,18 @@ def prepare_package_dirs() -> None:
 
 
 def process_file(args: TahweelArgumentParser, processor: GoogleDriveOcrProcessor, file_manager: PdfFileManager) -> None:
-  if not args.skip_output_check and file_manager.already_processed(args.tahweel_type, args.file_or_dir_path):
+  if (not args.skip_output_check and
+          file_manager.output_exists(args.tahweel_type, args.dir_output_type, args.file_or_dir_path)):
     return
 
   file_manager.to_images()
 
   with ThreadPoolExecutor(max_workers=args.processor_max_workers) as executor:
-    ocred_pages = list(executor.map(processor.process, file_manager.images_paths))
+    content = list(executor.map(processor.process, file_manager.images_paths))
 
-  ocred_pages = list(map(lambda text: text.replace('﻿________________', ''), ocred_pages))
-  ocred_pages = list(map(lambda text: text.replace('﻿', ''), ocred_pages))
-  ocred_pages = list(map(str.strip, ocred_pages))
+  content = list(map(lambda text: text.replace('﻿________________', ''), content))
+  content = list(map(lambda text: text.replace('﻿', ''), content))
+  content = list(map(str.strip, content))
 
-  TxtWriter(file_manager.txt_file_path(args.tahweel_type, args.file_or_dir_path)).write(ocred_pages)
-  DocxWriter(file_manager.docx_file_path(args.tahweel_type, args.file_or_dir_path)).write(ocred_pages)
+  TxtWriter(file_manager.txt_file_path(args.tahweel_type, args.dir_output_type, args.file_or_dir_path)).write(content)
+  DocxWriter(file_manager.docx_file_path(args.tahweel_type, args.dir_output_type, args.file_or_dir_path)).write(content)
--- tahweel/managers/pdf_file_manager.py
+++ tahweel/managers/pdf_file_manager.py
@@ -5,7 +5,7 @@ from pathlib import Path
 import pdf2image
 import platformdirs
 
-from tahweel.enums import TahweelType
+from tahweel.enums import DirOutputType, TahweelType
 
 
 MAX_FILE_SIZE_IN_BYTES = 4900000
@@ -41,22 +41,33 @@ class PdfFileManager:
           stdout=subprocess.DEVNULL,
         )
 
-  def already_processed(self, tahweel_type: TahweelType, dir: Path | None = None) -> bool:
-    return self.txt_file_path(tahweel_type, dir).exists() and self.docx_file_path(tahweel_type, dir).exists()
+  def output_exists(self, tahweel_type: TahweelType, dir_output_type: DirOutputType, dir: Path | None = None) -> bool:
+    return (self.txt_file_path(tahweel_type, dir_output_type, dir).exists() and
+            self.docx_file_path(tahweel_type, dir_output_type, dir).exists())
 
-  def txt_file_path(self, tahweel_type: TahweelType, dir: Path | None = None) -> Path:
-    return self._output_file_path('.txt', tahweel_type, dir)
+  def txt_file_path(self, tahweel_type: TahweelType, dir_output_type: DirOutputType, dir: Path | None = None) -> Path:
+    return self._output_file_path('.txt', tahweel_type, dir_output_type, dir)
 
-  def docx_file_path(self, tahweel_type: TahweelType, dir: Path | None = None) -> Path:
-    return self._output_file_path('.docx', tahweel_type, dir)
+  def docx_file_path(self, tahweel_type: TahweelType, dir_output_type: DirOutputType, dir: Path | None = None) -> Path:
+    return self._output_file_path('.docx', tahweel_type, dir_output_type, dir)
 
-  def _output_file_path(self, suffix: str, tahweel_type: TahweelType, dir: Path | None = None) -> Path:
+  def _output_file_path(
+    self,
+    suffix: str,
+    tahweel_type: TahweelType,
+    dir_output_type: DirOutputType,
+    dir: Path | None = None,
+  ) -> Path:
     match tahweel_type:
       case TahweelType.FILE:
         return self.file_path.with_suffix(suffix)
       case TahweelType.DIR:
+        if dir_output_type == DirOutputType.SIDE_BY_SIDE:
+          return self.file_path.with_suffix(suffix)
+
         if dir is None:
-          raise ValueError('`dir` is required when `tahweel_type` is `TahweelType.DIR`')
+          raise ValueError(
+            '`dir` is required when `tahweel_type` is `TahweelType.DIR` and `dir_output_type` is `DirOutputType.TREE_TO_TREE`')
 
         match suffix:
           case '.txt':
