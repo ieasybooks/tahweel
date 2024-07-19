e71506f00119e72e7759e901c2e867fdd3f42c34
Ali Hamdi Ali Fadel <aliosm1997@gmail.com>
2024-07-19 09:10:52 +0000

    Utilize PdfManager and GoogleDriveOcrProcessor in cli.py

--- tahweel/cli.py
+++ tahweel/cli.py
@@ -1,5 +1,27 @@
+from pathlib import Path
+
+import platformdirs
+
 from tahweel import TahweelArgumentParser
+from tahweel.enums import TahweelType
+from tahweel.managers import PdfManager
+from tahweel.processors import GoogleDriveOcrProcessor
 
 
 def main() -> None:
   args = TahweelArgumentParser(underscores_to_dashes=True).parse_args()
+
+  prepare_package_dirs()
+
+  processor = GoogleDriveOcrProcessor(args.service_account_credentials)
+
+  if args.tahweel_type == TahweelType.FILE:
+    pdf_manager = PdfManager(args.file_or_dir_path, args.pdf2image_thread_count)
+    pdf_manager.to_images()
+
+    for image_path in pdf_manager.images_paths:
+      print(processor.process(image_path))
+
+
+def prepare_package_dirs() -> None:
+  Path(platformdirs.user_cache_dir('Tahweel')).mkdir(parents=True, exist_ok=True)
