b7894eb9cb2c63918133304b8bc01369b64d0e03
Ali Hamdi Ali Fadel <aliosm1997@gmail.com>
2024-07-28 12:58:28 +0000

    Implement FileManagersFactory

---
+++ tahweel/managers/file_managers_factory.py
@@ -0,0 +1,28 @@
+from pathlib import Path
+
+from PIL import Image
+
+from tahweel.managers import BaseFileManager
+from tahweel.managers.image_file_manager import ImageFileManager
+from tahweel.managers.pdf_file_manager import PdfFileManager
+
+
+class FileManagersFactory:
+  @classmethod
+  def from_file_path(cls, file_path: Path, pdf2image_thread_count: int) -> BaseFileManager:
+    if file_path.suffix == '.pdf':
+      return PdfFileManager(file_path, pdf2image_thread_count)
+    elif cls._is_valid_image(file_path):
+      return ImageFileManager(file_path)
+    else:
+      raise ValueError(f'Unsupported file type: `{file_path.suffix}`.')
+
+  @classmethod
+  def _is_valid_image(cls, file_name: Path) -> bool:
+    try:
+      with Image.open(file_name) as img:
+        img.verify()
+
+      return True
+    except Exception:
+      return False
