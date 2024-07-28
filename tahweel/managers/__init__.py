c77ad15c873b3d03e0b55ba79d2ab1920bf6d5f5
Ali Hamdi Ali Fadel <aliosm1997@gmail.com>
2024-07-28 12:57:16 +0000

    Inherit PdfFileManager from BaseFileManager

--- tahweel/managers/__init__.py
+++ tahweel/managers/__init__.py
@@ -1,4 +1,5 @@
-__all__ = ['PdfFileManager']
+__all__ = ['BaseFileManager', 'PdfFileManager']
 
 
+from tahweel.managers.base_file_manager import BaseFileManager
 from tahweel.managers.pdf_file_manager import PdfFileManager
