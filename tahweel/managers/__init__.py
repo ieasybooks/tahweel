3bd0fdd03fe76ab4665214ba1df278e1aff6cbbb
Ali Hamdi Ali Fadel <aliosm1997@gmail.com>
2024-07-28 12:58:51 +0000

    Import ImageFileManager and FileManagersFactory in __init__.py

--- tahweel/managers/__init__.py
+++ tahweel/managers/__init__.py
@@ -1,5 +1,7 @@
-__all__ = ['BaseFileManager', 'PdfFileManager']
+__all__ = ['BaseFileManager', 'FileManagersFactory', 'ImageFileManager', 'PdfFileManager']
 
 
 from tahweel.managers.base_file_manager import BaseFileManager
+from tahweel.managers.file_managers_factory import FileManagersFactory
+from tahweel.managers.image_file_manager import ImageFileManager
 from tahweel.managers.pdf_file_manager import PdfFileManager
